15945c99ed41e49f8c712aaf1cbd51db7079a946
Saad Reyan Khan <saadreyan0@gmail.com>
2026-09-08 01:36:47 -0700

    add modren HTML and CSS for UI design

--- DataType Checker/script.js
+++ DataType Checker/script.js
@@ -1,22 +1,32 @@
-let input = prompt("Enter a value:");
 
-let value;
+function checkValue() {
 
-if (input === "null") {
-    value = null;
-} 
-else if (input === "true") {
-    value = true;
-} 
-else if (input === "false") {
-    value = false;
-} 
-else if (input !== "" && !isNaN(input)) {
-    value = Number(input);
-} 
-else {
-    value = input;
+    let input = prompt("Enter a value:");
+
+    let value;
+
+    if (input === "null") {
+        value = null;
+    }
+    else if (input === "true") {
+        value = true;
+    }
+    else if (input === "false") {
+        value = false;
+    }
+    else if (input !== "" && !isNaN(input)) {
+        value = Number(input);
+    }
+    else {
+        value = input;
+    }
+
+    console.log("Value:", value);
+    console.log("Data Type:", typeof value);
+
+    // Display result on webpage
+    document.getElementById("valueOutput").textContent = String(value);
+
+    document.getElementById("typeOutput").textContent = typeof value;
 }
 
-console.log("Value:", value);
-console.log("Data Type:", typeof value);
